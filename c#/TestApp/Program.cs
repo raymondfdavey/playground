7a335272577589a4f5f4fb24fd39d31387f20809
raymondfdavey <raymondfdavey@gmail.com>
2022-06-24 16:07:50 +0100

    wip

--- c#/TestApp/Program.cs
+++ c#/TestApp/Program.cs
@@ -6,10 +6,7 @@ namespace TestApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string yourName;
-            Console.WriteLine("What is your name?");
-            yourName = Console.ReadLine();
-            Console.WriteLine("Hello {0}", yourName);        
+    
         }
     }
 }
